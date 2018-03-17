55f530a5ce6de751667d063ca7e792d2ba6ddcdd
toppomameta <toppo850122@gmail.com>
2018-03-18 08:35:04 +0900

    add customer model

--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SpartaCrm
+module SpartCrm
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo' # タイムゾーンもついでに変更
+    config.i18n.default_locale = :ja
   end
 end
